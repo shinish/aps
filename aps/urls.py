a5331c56151c55552c7753b5a570836da1c2dc4f
Shinish Sasidharan <shinish@live.com>
2022-10-05 05:36:09 +0530

    Bug static

--- aps/urls.py
+++ aps/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('webapp/', include('webapp.urls')),
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
-
-
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
